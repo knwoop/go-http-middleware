84b8e3a5428bfe5e973970c0561e476ee107a1c4
kenta takahashi <kenta.takahashi@showcase-gig.com>
2020-09-15 09:06:18 +0900

    fix example

--- _example/main.go
+++ _example/main.go
@@ -22,28 +22,34 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *server) registerRoutes() {
-	s.mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-}
-
-func main() {
 	adapters := []adapter.Adapter{
 		csrf.SetTokenAdapter(),
 		csrf.SetHeaderAdapter(),
 		cors.Adapter(),
 	}
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	}
+
+	s.mux.Handle("/", adapter.Apply(http.HandlerFunc(fn), adapters...))
+	s.mux.Handle("/noadapter", http.HandlerFunc(fn))
+}
+
+func main() {
+
 	s := &server{mux: http.NewServeMux()}
 	s.registerRoutes()
-	server := &http.Server{Handler: adapter.Apply(s, adapters...)}
+	server := &http.Server{Handler: s}
 
 	lis, err := net.Listen("tcp", fmt.Sprintf("%s:%d", "127.0.0.1", 9090))
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "[ERROR] net listen %s", err)
+		os.Exit(1)
 	}
 
 	if err := server.Serve(lis); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "[ERROR] fail to serve %s", err)
+		os.Exit(1)
 	}
-	os.Exit(1)
+	os.Exit(0)
 }
